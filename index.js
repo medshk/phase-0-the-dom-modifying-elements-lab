f7fc3cc54c4ea1eded090bcf6882c542876ab866
med <mohammed.bennaceur98@gmail.com>
2023-09-02 22:58:33 +0100

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+
+document.body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Mohammed is the champion";
+document.body.append(newHeader);
